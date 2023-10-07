e889f21eb555b8b88767966b8c8063c66bf82a19
hulei <hulei@antiy.cn>
2023-10-07 16:04:26 +0800

    shiro：shiro基本搭建完成，下一步，mybatis连接数据库

---
+++ src/main/java/com/antiy/hulei/configuration/shiro/ShiroConfig.java
@@ -0,0 +1,55 @@
+package com.antiy.hulei.configuration.shiro;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+    //ShiroFilterFactoryBean
+    @Bean("shiroFilterFactoryBean")
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean(@Qualifier("securityManager") DefaultWebSecurityManager defaultWebSecurityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        // 设置安全管理器
+        shiroFilterFactoryBean.setSecurityManager(defaultWebSecurityManager);
+
+        // 添加shiro的内置过滤器
+        /*
+            anon：无需认证就可以访问
+            authc：必须认证才可以访问
+            user：必须有“记住我”功能才能使用
+            perms：拥有对某个资源的权限才能访问
+            role：拥有某个角色权限才能访问
+         */
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        filterChainDefinitionMap.put("/user/*", "authc");
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+
+        // 设置登录的请求
+        shiroFilterFactoryBean.setLoginUrl("/toLogin");
+        return shiroFilterFactoryBean;
+    }
+
+    //DefaultWebSecurityManager
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager(@Qualifier("userRealm")UserRealm userRealm){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 关联UserRealm
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+
+
+    //realm
+    @Bean(name = "userRealm")
+    public UserRealm userRealm() {
+        return new UserRealm();
+    }
+}
--- src/main/java/com/antiy/hulei/controller/IndexController.java
+++ src/main/java/com/antiy/hulei/controller/IndexController.java
@@ -1,23 +1,58 @@
 package com.antiy.hulei.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
 public class IndexController {
 
+    //首页
     @RequestMapping({"/", "/index"})
     public String hello(Model model){
-        model.addAttribute("msg", "首页");
-        return "index";
+        return "main";
     }
 
-    @RequestMapping("/main")
+    //跳转登录
+    @RequestMapping("/toLogin")
     public String utliboxIndex(Model model) {
-        return "main";
+        model.addAttribute("msg", "请登录！");
+        return "login";
+    }
+
+    //登录请求
+    @RequestMapping("/login")
+    public String login(String username, String password, Model model){
+        // 获取当前的用户
+        Subject subject = SecurityUtils.getSubject();
+        // 封装用户的登录数据
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken(username, password);
+        // 执行登录的方法，如果没有异常就说明ok
+        try{
+            subject.login(usernamePasswordToken);
+            return "index";
+        }catch (UnknownAccountException e){
+            model.addAttribute("msg", "用户名错误");
+            return "login";
+        }catch (IncorrectCredentialsException e){
+            model.addAttribute("msg","密码错误");
+            return "login";
+        }
+    }
+
+    @RequestMapping("/user/add")
+    public String add(){
+        return "user/add";
+    }
+
+    @RequestMapping("/user/update")
+    public String update(){
+        return "user/update";
     }
 }
